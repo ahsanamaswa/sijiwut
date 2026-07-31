62fc8c5112376afa72c78f7c8a733c05511d7a74
ahsanamaswa <ahsanamaswa2519@gmail.com>
2026-07-31 13:33:56 +0700

    Add manual sitemap route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Berita;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TentangDesaController;
 use App\Http\Controllers\PetaController;
@@ -20,6 +21,30 @@ Route::get('/peta', [PetaController::class, 'index'])->name('peta');
 Route::get('/galeri', [GaleriController::class, 'index'])->name('galeri.index');
 Route::get('/berita', [BeritaController::class, 'index'])->name('berita.index');
 Route::get('/berita/{slug}', [BeritaController::class, 'show'])->name('berita.show');
+
+// Sitemap untuk SEO
+Route::get('/sitemap.xml', function () {
+    $urls = collect([
+        ['loc' => route('home'), 'priority' => '1.0', 'changefreq' => 'weekly'],
+        ['loc' => route('tentang-desa'), 'priority' => '0.8', 'changefreq' => 'monthly'],
+        ['loc' => route('peta'), 'priority' => '0.7', 'changefreq' => 'monthly'],
+        ['loc' => route('galeri.index'), 'priority' => '0.7', 'changefreq' => 'weekly'],
+        ['loc' => route('berita.index'), 'priority' => '0.8', 'changefreq' => 'daily'],
+    ]);
+
+    Berita::all()->each(function ($berita) use ($urls) {
+        $urls->push([
+            'loc' => route('berita.show', $berita->slug),
+            'priority' => '0.6',
+            'changefreq' => 'monthly',
+            'lastmod' => $berita->updated_at->toAtomString(),
+        ]);
+    });
+
+    return response()
+        ->view('sitemap', compact('urls'))
+        ->header('Content-Type', 'text/xml');
+})->name('sitemap');
 
 // Redirect bawaan Breeze setelah login berhasil
 Route::get('/dashboard', function () {
